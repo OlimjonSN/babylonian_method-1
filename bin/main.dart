274d10781564871043c91cd66b3ef8ef3df33c99
olimjonsn230 <olimjonsn230@gmail.com>
2022-07-05 11:46:36 +0500

    done

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,16 @@
 // babyloniam method
 // crate function named func with arguments a and b
 // The variables are S and d given
+import 'dart:math';
 
 void main() {}
+func(a, b) {
+  int S = 24;
+  int d = 6;
+  double a = (S - pow(d, 2)) / (2 * d);
+
+  double b = a + d;
+
+  double x = b - (pow(a, 2) / (2 * b));
+  return x;
+}
